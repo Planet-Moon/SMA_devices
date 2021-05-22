fdc81d7a3419f8a8bce996e157ac1f6be7798677
Planet-Moon <topwastaken@abwesend.de>
2021-05-22 02:20:24 +0200

    [feature] add battery manager

--- SMA_StorageBoy.py
+++ SMA_StorageBoy.py
@@ -152,3 +152,119 @@ class SMA_StorageBoy(SMA_device):
     @property
     def maxDischargePower(self):
         return self.get_data("maxDischargePower")
+
+class BatteryManager:
+    supported_inverters = (SMA_StorageBoy) # add supported_inverters here
+    def __init__(self, inverters:list=[]) -> None:
+        self._inverters = []
+        for i in inverters:
+            self.addInverter(i)
+
+    @property
+    def inverters(self) -> list:
+        return self._inverters
+
+    def addInverter(self, inverter) -> None:
+        """Add inverter to BatteryManager
+
+        Args:
+            inverter (self.supported_inverters): Any inverter type in supported_inverters
+
+        Raises:
+            TypeError: When inverter is not in the tuple of supported inverters
+        """
+        if isinstance(inverter,self.supported_inverters):
+            self._inverters.append(inverter)
+        else:
+            raise TypeError("Inverter not supported")
+
+    @property
+    def power(self) -> int:
+        """Calculate the power needed or available
+
+        Returns:
+            int: power needed (<0) or available (>0) (W)
+        """
+        needed_power = 0
+        available_power = 0
+        for i in self._inverters:
+            if i.AktuellerBatterieladezustand < 65:
+                needed_power += i.maxChargePower
+            if i.AktuellerBatterieladezustand > 85:
+                available_power += 2000
+        if needed_power > 0:
+            return needed_power *-1
+        else:
+            return available_power
+
+    @property
+    def soc(self) -> float:
+        """Mean state of charge
+
+        Returns:
+            float: Mean state of charge (%)
+        """
+        _soc = 0
+        for i in self._inverters:
+            _soc += i.AktuellerBatterieladezustand
+        return _soc/len(self._inverters)
+
+    @property
+    def present_discharge(self) -> int:
+        """Present amount of power being discharged
+
+        Returns:
+            int: discharging power (W)
+        """
+        _present_discharge = 0
+        for i in self._inverters:
+            _present_discharge += i.Batterieentladung
+        return _present_discharge
+
+    @property
+    def present_charge(self) -> int:
+        """Present amount of power being charged
+
+        Returns:
+            int: charging power (W)
+        """
+        _present_charge = 0
+        for i in self._inverters:
+            _present_charge += i.Batterieladung
+        return _present_charge
+
+    @property
+    def charge(self) -> int:
+        """Charged energy in the batteries
+
+        Returns:
+            int: charged energy (Wh)
+        """
+        _charge = 0
+        for i in self._inverters:
+            _charge += i.Batterieladung
+        return _charge
+
+    @property
+    def total_capacity(self) -> int:
+        """Total nominal capacity of all batteries
+
+        Returns:
+            int: Nominal capacity of all batteries (Wh)
+        """
+        _cap = 0
+        for i in self._inverters:
+            _cap += i.nomCapacity
+        return _cap
+
+    @property
+    def charge_missing(self) -> int:
+        """Total charge missing until batteries are full
+
+        Returns:
+            int: charge missing (Wh)
+        """
+        _charge_missing = 0
+        for i in self._inverters:
+            _charge_missing += i.nomCapacity - i.MomentaneBatterieladung
+        return _charge_missing
