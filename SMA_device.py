d42174e14b7d36f41ba59834c7a1c50caf2c8eef
Planet-Moon <topwastaken@abwesend.de>
2021-05-30 22:44:19 +0200

    [style] change to pep8

--- SMA_device.py
+++ SMA_device.py
@@ -5,8 +5,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class SMA_device(modbus_device):
-    def __init__(self, ipAddress:str, port:str=""):
+    def __init__(self, ipAddress: str, port: str = ""):
         """Generic SMA inverter class
 
         Args:
@@ -21,21 +22,23 @@ class SMA_device(modbus_device):
         self.error = 0
         self.ipAddress = ipAddress
         self.port = port
-        super().__init__(ipAddress,port)
+        super(SMA_device, self).__init__(ipAddress, port)
         self.modbus_read_all = modbus_device.read_all
         self._Init_Modbus_Registers()
 
         try:
             self.get_data("all")
         except Exception as e:
-            logger.error("Error - Initialisation of "+self.ipAddress+" failed!, "+str(e))
+            logger.error("Error - Initialisation of " +
+                         self.ipAddress+" failed!, "+str(e))
         pass
 
     def _Init_Modbus_Registers(self):
 
         self.newRegister("UnitID", address=42109, length=4)
         UnitID_response = self.read("UnitID")
-        self.physicalSerialNumber = TC.list_to_number([UnitID_response[0], UnitID_response[1]], signed=False)
+        self.physicalSerialNumber = TC.list_to_number(
+            [UnitID_response[0], UnitID_response[1]], signed=False)
         self.physicalSusyID = UnitID_response[2]
         self.UnitID = UnitID_response[3]
         self.UnitID = UnitID_response[3]
@@ -44,23 +47,27 @@ class SMA_device(modbus_device):
 
         self.newRegister("serialnumber", address=30057, length=2, signed=False)
 
-        self.newRegister("NameplateSerialnumber", address=30057, length=2, signed=False)
+        self.newRegister("NameplateSerialnumber",
+                         address=30057, length=2, signed=False)
 
         self.newRegister("Model", address=30053, length=2, signed=False)
 
-        self.newRegister("FirmwareVersion", address=40063, length=2, signed=False)
+        self.newRegister("FirmwareVersion", address=40063,
+                         length=2, signed=False)
 
         self.newRegister("timeZone", address=40003, length=2)
 
         self.newRegister("operationHealth", address=30201, length=2)
 
-        self.newRegister("SpeedwireEnable", address=40157, length=2, signed=False, type_="int", unit="")
+        self.newRegister("SpeedwireEnable", address=40157,
+                         length=2, signed=False, type_="int", unit="")
 
         self.read_all()
         pass
 
     def reboot(self):
-        self.newRegister("reboot", address=40077, length=2, signed=False, type_="int", unit="")
+        self.newRegister("reboot", address=40077, length=2,
+                         signed=False, type_="int", unit="")
         self.write_register("reboot", 1146)
         self.removeRegister("reboot")
 
